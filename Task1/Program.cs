b71adeba8504599ecd136320d204b76c3fcc6c34
Natali <salamatina_nata@mail.ru>
2022-10-03 22:40:20 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. напишите прогу, которая выведет все натуральные числа в промежутке от 1 до n
+
+Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(PrintNumber(num, 1));
+string PrintNumber(int start, int end)
+{
+    if(start == end)
+    {
+        return start.ToString();
+        // сначала пишем окончание рекурсии
+    }
+    return (start + "," + PrintNumber(start - 1, end));
+    // потом сама рекурсия
+}
+
